2254d33f2b2c4492abf36795f6b6aa1fc2ebb525
Hafiz Adewuyi <hoadewuyi@gmail.com>
2024-01-15 23:24:06 +0200

    Rename Dto folders

---
+++ Api/Model/Response/NameEntryDto.cs
@@ -0,0 +1,29 @@
+﻿using Api.Model.In;
+using Core.Enums;
+
+namespace Api.Model.Out
+{
+    public record NameEntryDto
+    {
+        public string Name { get; set; }
+        public string Meaning { get; set; }
+        public string? Pronunciation { get; set; }
+        public string? IpaNotation { get; set; }
+        public CommaSeparatedString Variants { get; set; }
+        public HyphenSeparatedString Syllables { get; set; }
+        public string? ExtendedMeaning { get; set; }
+        public CommaSeparatedString Morphology { get; set; }
+
+        public List<GeoLocationDto> GeoLocation { get; set; }
+
+        public CommaSeparatedString FamousPeople { get; set; }
+        public CommaSeparatedString Media { get; set; }
+        public string SubmittedBy { get; set; }
+
+        public List<EtymologyDto> Etymology { get; set; }
+
+        public State State { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
