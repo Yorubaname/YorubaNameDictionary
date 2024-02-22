6640a67b0de823033e8d1b186238f831c550127f
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-02-22 14:14:20 +0100

    feature: add GeoLocations Controller

---
+++ Api/Controllers/GeoLocationsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class GeoLocationsController : ControllerBase
+    {
+        [Route("/geolocations")]
+        [HttpGet]
+        public IActionResult ListGeoLocations()
+        {
+            return Ok();
+        }
+    }
+}
