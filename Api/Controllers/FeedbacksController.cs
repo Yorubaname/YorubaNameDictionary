3f81ead19eee1b27aab8dd0c81c022f5d8db192c
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-02 19:27:01 +0100

    add feedaback controller

---
+++ Api/Controllers/FeedbacksController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class FeedbacksController : ControllerBase
+    {
+    }
+}
