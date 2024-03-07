da3ac1d7f60d90b93d72e7d3511c3f162d6104fe
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-07 15:27:25 +0100

    add name feedback interface and repository

---
+++ Core/Repositories/INameEntryFeedbackRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entities.NameEntry.Collections;
+namespace Core.Repositories
+{
+    public interface INameEntryFeedbackRepository
+    {
+        Task<List<Feedback>> FindAll(string sort);
+        Task<List<Feedback>> FindByName(string name, string sort);
+    }
+}
---
+++ Infrastructure.MongoDB/Repositories/NameEntryFeedbackRepository.cs
@@ -0,0 +1,46 @@
+﻿using Core.Entities.NameEntry;
+using Core.Entities.NameEntry.Collections;
+using Core.Repositories;
+using MongoDB.Driver;
+
+namespace Infrastructure.MongoDB.Repositories;
+
+public class NameEntryFeedbackRepository : INameEntryFeedbackRepository
+{
+    private readonly IMongoCollection<NameEntry> _nameEntryCollection;
+
+    public NameEntryFeedbackRepository(IMongoDatabase database)
+    {
+        _nameEntryCollection = database.GetCollection<NameEntry>("NameEntries");
+    }
+
+    public async Task<List<Feedback>> FindAll(string sortOrder)
+    {
+        var sortDefinition = sortOrder.Equals("desc", System.StringComparison.OrdinalIgnoreCase)
+            ? Builders<NameEntry>.Sort.Descending("Feedbacks.CreatedAt")
+            : Builders<NameEntry>.Sort.Ascending("Feedbacks.CreatedAt");
+
+        var projectionDefinition = Builders<NameEntry>.Projection.Include("Feedbacks.Content").Exclude("_id");
+
+        return await _nameEntryCollection
+            .Find(_ => true)
+            .Sort(sortDefinition)
+            .Project<Feedback>(projectionDefinition)
+            .ToListAsync();
+    }
+
+    public async Task<List<Feedback>> FindByName(string name, string sortOrder)
+    {
+        var sortDefinition = sortOrder.Equals("desc", System.StringComparison.OrdinalIgnoreCase)
+            ? Builders<NameEntry>.Sort.Descending("Feedbacks.CreatedAt")
+            : Builders<NameEntry>.Sort.Ascending("Feedbacks.CreatedAt");
+
+        var projectionDefinition = Builders<NameEntry>.Projection.Include("Feedbacks.Content").Exclude("_id");
+
+        return await _nameEntryCollection
+            .Find(entry => entry.Name == name)
+            .Sort(sortDefinition)
+            .Project<Feedback>(projectionDefinition)
+            .ToListAsync();
+    }
+}
