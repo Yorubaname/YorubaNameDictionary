755cdd661a1c0f9fcd917cdf09628ba602979a4c
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-31 10:13:10 +0100

    add GetSuggestedMetaData endpoint

--- Api/Controllers/SuggestedNamesController.cs
+++ Api/Controllers/SuggestedNamesController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SuggestedNamesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SuggestedNamesController : ControllerBase
+    private readonly SuggestedNameService _suggestedNameService;
+
+    public SuggestedNamesController(SuggestedNameService suggestedNameService)
     {
+        _suggestedNameService = suggestedNameService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetSuggestedMetaData()
+    {
+        var suggestname = await _suggestedNameService.CountAsync();
+
+        return Ok(suggestname);
     }
 }
