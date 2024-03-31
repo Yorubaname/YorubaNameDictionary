ad9a524e97ce5235ad5c043e46d0ff89177a6512
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-31 10:59:29 +0100

    add suggestedName dto

---
+++ Core/Dto/Response/SuggestedNameDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Dto.Request;
+
+namespace Core.Dto.Response;
+
+public record SuggestedNameDto
+{
+    public string? Name { get; set; }
+    public string? Details { get; set; }
+    public List<GeoLocationDto> GeoLocation { get; set; }
+    public string? Email { get; set; }
+
+    public SuggestedNameDto()
+    {
+        GeoLocation = new List<GeoLocationDto>();
+    }
+}
