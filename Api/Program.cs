3bf20df4aab92ba2dc1030bcc7fc4a2664ab7a87
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-31 09:58:44 +0100

    add and register suggested name service

--- Api/Program.cs
+++ Api/Program.cs
@@ -32,6 +32,7 @@ services.AddScoped<NameEntryService>();
 services.AddScoped<GeoLocationsService>();
 services.AddScoped<IEventPubService, EventPubService>();
 services.AddScoped<SearchService>();
+services.AddScoped<SuggestedNameService>();
 
 
 var app = builder.Build();
---
+++ Application/Services/SuggestedNameService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Repositories;
+
+namespace Application.Services;
+
+public class SuggestedNameService
+{
+    private readonly ISuggestedNameRepository _suggestedNameRepository;
+
+    public SuggestedNameService(ISuggestedNameRepository suggestedNameRepository)
+    {
+        _suggestedNameRepository = suggestedNameRepository;
+    }
+
+    public async Task<Dictionary<string, int>> Count()
+    {
+        return await _suggestedNameRepository.Count();
+    }
+}
