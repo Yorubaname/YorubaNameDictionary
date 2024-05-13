d983ab4e8606e6ea690c3f377165710103dd563a
olusheenor <ayeniolusina@gmail.com>
2024-05-13 10:12:00 -0400

    Adding deletename event handlers

---
+++ Application/EventHandlers/NameDeletedEventHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Events;
+using Application.Services;
+using Core.Cache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.EventHandlers
+{
+
+    public class NameDeletedEventHandler : INotificationHandler<NameDeletedAdapter>
+    {
+        private INameDeletedCache _nameDeletedCache;
+        private IRecentIndexesCache _recentIndexesCache;
+        private IRecentSearchesCache _recentSearchesCache;
+
+        public NameDeletedEventHandler(INameDeletedCache nameDeletedCache, IRecentIndexesCache recentIndexesCache, IRecentSearchesCache recentSearchesCache)
+        {
+            _nameDeletedCache = nameDeletedCache;
+            _recentIndexesCache = recentIndexesCache;
+            _recentSearchesCache = recentSearchesCache;
+        }
+
+        public async Task Handle(NameDeletedAdapter notification, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _recentIndexesCache.Remove(notification.Name);
+                await _recentSearchesCache.Remove(notification.Name);
+                await _nameDeletedCache.Stack(notification.Name); // not sure about this line just following conventions..
+            }
+            catch (Exception ex)
+            {
+                //TODO log this
+            }
+        }
+    }
+}
---
+++ Core/Dto/Response/IndexOperationStatusDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dto.Response
+{
+    public record IndexOperationStatusDto { 
+        public IndexOperationStatusDto(bool status, string message)
+        {
+            Status = status;
+            Message = message;
+        }
+        public bool Status { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
