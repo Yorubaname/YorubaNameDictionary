32f341266fa2ea4a3b9cf459072db7367c524648
Hafiz Adewuyi <hoadewuyi@gmail.com>
2024-06-11 15:01:38 +0300

    Authenticate and fix Feedbacks controller.

--- Core/Repositories/INameEntryFeedbackRepository.cs
+++ Core/Repositories/INameEntryFeedbackRepository.cs
@@ -1,13 +1,14 @@
-﻿using Core.Entities.NameEntry.Collections;
+﻿using Core.Dto.Response;
+using Core.Entities.NameEntry.Collections;
 namespace Core.Repositories
 {
     public interface INameEntryFeedbackRepository
     {
-        Task<List<Feedback>> FindAllAsync();
-        Task<List<Feedback>> FindByNameAsync(string name);
+        Task<List<FeedbackDto>> FindAllAsync();
+        Task<List<FeedbackDto>> FindByNameAsync(string name);
         Task AddFeedbackByNameAsync(string name, string feedbackContent);
         Task DeleteAllFeedbackForNameAsync(string name);
-        Task<Feedback> GetFeedbackByIdAsync(string feedbackId);
+        Task<FeedbackDto> GetFeedbackByIdAsync(string feedbackId);
         Task<bool> DeleteFeedbackAsync(string name, string feedbackId);
     }
 }
