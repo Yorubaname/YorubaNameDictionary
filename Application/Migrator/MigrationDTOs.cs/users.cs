853656fe8b652b86b0955d94df91825036a5b238
Olusheenor <shayman4real@gmail.com>
2024-06-12 19:25:12 -0400

    Added migrate users

---
+++ Application/Migrator/MigrationDTOs.cs/users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Migrator.MigrationDTOs.cs
+{
+    internal class users
+    {
+        public string? email { get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }
+        public string roles { get; set; }
+
+    }
+}
