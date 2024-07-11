2babba4815f8ee57084578bbba95fc51198b55c6
Hafiz Adewuyi <hoadewuyi@gmail.com>
2024-07-11 19:06:34 +0300

    Remove TODO comment.

--- Core/Dto/Request/GeoLocationDto.cs
+++ Core/Dto/Request/GeoLocationDto.cs
@@ -1,11 +1,7 @@
 ﻿namespace Core.Dto.Request
 {
-    /// <summary>
-    /// TODO: Add field validations
-    /// </summary>
     public record GeoLocationDto(string Place, string Region)
     {
-
         public override string ToString() 
         {
             return Place;
