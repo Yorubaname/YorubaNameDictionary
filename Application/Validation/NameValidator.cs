c9f76766939a7b232701f07f43b4c11d2540b361
Olusheenor <shayman4real@gmail.com>
2024-07-20 21:35:28 -0400

    added suggested name validation

--- Application/Validation/NameValidator.cs
+++ Application/Validation/NameValidator.cs
@@ -5,10 +5,16 @@ namespace Application.Validation
 {
     public class NameValidator : AbstractValidator<NameDto>
     {
+
+      
+
         public NameValidator(GeoLocationValidator geoLocationValidator, EmbeddedVideoValidator embeddedVideoValidator, EtymologyValidator etymologyValidator)
         {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
             RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Name is required.");
+            .NotEmpty().WithMessage("Name is required.")
+            .Length(2, 40).WithMessage("Name must be 2 to 40 chracters");
 
             RuleFor(x => x.Meaning)
                 .NotEmpty().WithMessage("Meaning is required.");
--- Core/Dto/Request/CreateSuggestedNameDto.cs
+++ Core/Dto/Request/CreateSuggestedNameDto.cs
@@ -1,19 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace Core.Dto.Request;
 
 public record CreateSuggestedNameDto
-{
-    [Required]
-    public string Name { get; init; }
-
-    [Required]
+{  
+    public string Name { get; init; }   
     public string Details { get; init; }
-
-    [Required]
     public string Email { get; init; }
     public List<GeoLocationDto> GeoLocation { get; set; }
-
     public CreateSuggestedNameDto()
     {
         GeoLocation = new List<GeoLocationDto>();
