4ddba85d890faa555e06a9883b8b42783b20c63f
Olusheenor <shayman4real@gmail.com>
2024-07-20 21:37:57 -0400

    formatting code

--- Application/Validation/CreateSuggestedNameValidator.cs
+++ Application/Validation/CreateSuggestedNameValidator.cs
@@ -5,16 +5,22 @@ using FluentValidation;
 
 namespace Application.Validation
 {
-    public class CreateSuggestedNameValidator :AbstractValidator<CreateSuggestedNameDto>
+    public class CreateSuggestedNameValidator : AbstractValidator<CreateSuggestedNameDto>
     {
         public CreateSuggestedNameValidator(GeoLocationValidator geoLocationValidator)
         {
             RuleLevelCascadeMode = CascadeMode.Stop;
 
             RuleFor(u => u.Name).NotEmpty().WithMessage("Name is required")
-                .Length(2,40).WithMessage("Name must be 2 to 40 characters");
-            RuleFor(u => u.Email).NotEmpty().WithMessage("Email is required").EmailAddress().WithMessage("Please enter a valid email address");
+                                .Length(2, 40).WithMessage("Name must be 2 to 40 characters");
+
+            RuleFor(u => u.Email).NotEmpty().WithMessage("Email is required")
+                                 .EmailAddress().WithMessage("Please enter a valid email address");
+
+
             RuleFor(u => u.Details).NotEmpty().WithMessage("Details is required");
+
+
             RuleForEach(u => u.GeoLocation).SetValidator(geoLocationValidator);
         }
     }
