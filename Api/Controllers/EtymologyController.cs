0d99b7135b7bbb99876eebee3b30a276a7e19a4e
Hafiz Adewuyi <hoadewuyi@gmail.com>
2024-07-24 12:46:08 +0300

    Fix route for Etymology controller.

--- Api/Controllers/EtymologyController.cs
+++ Api/Controllers/EtymologyController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class EtymologyController : ControllerBase
     {
