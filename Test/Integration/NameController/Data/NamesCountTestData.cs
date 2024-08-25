539d16fb381deb299c06013128992158a83cbcc4
Hafiz Adewuyi <hoadewuyi@gmail.com>
2024-08-25 14:34:36 +0300

    Use AutoFixutre to generate data for one more class.

--- Test/Integration/NameController/Data/NamesCountTestData.cs
+++ Test/Integration/NameController/Data/NamesCountTestData.cs
@@ -9,18 +9,18 @@ namespace Test.Integration.NameController.Data;
 public class NamesCountTestData : IEnumerable<object[]>
 {
     private readonly IFixture _fixture;
-    private readonly List<string> _nameSequence = new List<string> { "Ibironke", "Aderonke", "Olumide", "Akinwale", "Oluwaseun" };
+    private readonly List<string> _nameSequence = ["Ibironke", "Aderonke", "Olumide", "Akinwale", "Oluwaseun"];
     private int _nameIndex = 0;
 
     public NamesCountTestData()
     {
         _fixture = new Fixture();
-        // Customize other properties if necessary
+
         _fixture.Customize<NameEntry>(c => c
             .With(ne => ne.State, State.PUBLISHED)
             .With(ne => ne.Modified, (NameEntry?)default)
             .With(ne => ne.Duplicates, [])
-            .Do(ne => ne.Name = GetNextName())); // Generate names from a sequence
+            .Do(ne => ne.Name = GetNextName()));
     }
 
     public IEnumerator<object[]> GetEnumerator()
