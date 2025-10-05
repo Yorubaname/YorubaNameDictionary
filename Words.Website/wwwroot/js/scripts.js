5e2c0f5b8ff26eb3846714b470c09c1f520b5b30
Hafiz Adewuyi <hoadewuyi@gmail.com>
2025-10-05 17:07:50 +0100

    Fix UI issues: Search in homepage not working.

--- Words.Website/wwwroot/js/scripts.js
+++ Words.Website/wwwroot/js/scripts.js
@@ -132,7 +132,7 @@ $(function () {
             datumTokenizer: Bloodhound.tokenizers.whitespace,
             queryTokenizer: Bloodhound.tokenizers.whitespace,
             remote: {
-                url: '/v1/search/autocomplete?q=%QUERY',
+                url: '/api/v1/words/search/autocomplete?q=%QUERY',
                 wildcard: '%QUERY'
             }
         });
